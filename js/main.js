0b6863a053a2f46f1b80004eaede27df2ce73bd9
Patrick Kimanthi <kimanthip@gmail.com>
2021-06-10 20:57:29 +0300

    add comments to code

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'A
 
 let counter = currentYear;
 let i = 0;
-// Get years form 1900 to current year
+// Get and append (to selectYear html element) years form 1900 to current year
 while (counter >= 1900){ 
     let option = document.createElement('option');
     
@@ -19,20 +19,21 @@ while (counter >= 1900){
     counter --;
     i++;
 }
-// Push the months array value to selectMonth html element
+// Append the months array value to selectMonth html element
 for(i = 0; i < months.length; i++){
     let option = document.createElement('option');
     option.value = i;
     option.textContent = months[i];
     selectMonth.appendChild(option);
 }
-//
+// Check if selected year is a leap year
 let monthDays = [];
 let checkIfLeapYear = (selectedYear) => {
     if(((selectedYear % 4 === 0) && (selectedYear % 100 !== 0 )) || (selectedYear % 400 === 0)){
         // Leap year
         monthDays = ['31', '29', '31', '30', '31', '30', '31', '31', '30', '31', '30', '31'];
     } else{
+        // Its not a leap year
         monthDays = ['31', '28', '31', '30', '31', '30', '31', '31', '30', '31', '30', '31'];
     } 
 }
